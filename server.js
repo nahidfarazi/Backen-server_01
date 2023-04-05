85183f1ff5575edc262385a9c442a4d80c7fcd6f
nahidfarazi <nahidfarazi19@gmail.com>
2023-04-05 12:06:44 +0600

    server create

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const http = require('http');
+const app = require('./app/app');
+
+const PORT = process.env.PORT || 8000
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`);
+})
+
